b8ee4e7b8923a41527da043fa81ae31ee6dd7377
Jackson Socolofsky <jtsoco@gmail.com>
2023-04-25 15:49:06 +0900

    can upload a pdf, and the pdf text is put into a single text using a method

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,28 @@ class BooksController < ApplicationController
     reader = PDF::Reader.new(file)
 
     puts reader.info
-    # raise
+
+    text_full = read_pdf_pages(reader)
+    # TODO count the words
+    # eliminate duplicates
+    # eliminiate common words
+    # translate words
+    # write to csv
+    # give csv download link to individuals
+    # change front-end to send javascript post
+    # change backend to send json reply
+    # allow new csv to be downloaded, json reply lets that happen
+    # delete csv
+    raise
     file.unlink
   end
+
+  def read_pdf_pages(reader)
+    full_text = ""
+    reader.pages.each do |page|
+      full_text += (" " + page.text)
+    end
+    full_text
+  end
+
 end
